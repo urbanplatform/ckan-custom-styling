dfa2bfbfbb375da3962911b65770a020802cad04
Isac <icsilva@ubiwhere.com>
2022-03-11 11:47:20 +0000

    Ckan style

--- src/ckanext-style_granada/ckanext/style_granada/public/base/js/ckan.js
+++ src/ckanext-style_granada/ckanext/style_granada/public/base/js/ckan.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
 
     var w_url = window.location.href.split('/')[3];
 
+    if(!w_url){
+        $('form.lang-select').before('<div class="newLang"><img src="'+window.location.origin +'/base/images/paises/es.svg"></div>')
+    }
+
+    if(w_url)
     $('form.lang-select').before('<div class="newLang"><img src="'+window.location.origin +'/base/images/paises/'+ w_url +'.svg"></div>')
 
 });
