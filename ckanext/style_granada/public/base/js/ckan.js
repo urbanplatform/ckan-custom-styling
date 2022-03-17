b2ef6821434bf6881cddc39c372fc7a499c05788
Isac <icsilva@ubiwhere.com>
2022-03-17 16:18:06 +0000

    fix modal api

--- ckanext/style_granada/public/base/js/ckan.js
+++ ckanext/style_granada/public/base/js/ckan.js
@@ -13,4 +13,18 @@ $(document).ready(function(){
     if(w_url && w_url != 'dataset' && w_url != 'group' && w_url != 'about' && w_url != 'organization')
     $('form.lang-select').before('<div class="newLang"><img src="'+window.location.origin +'/base/images/paises/'+ w_url +'.svg"></div>')
 
+    $('a.btn-success[data-module="api-info"]').click(function() {
+        $(document).ajaxStop(function () {
+            if($(this).hasClass('newBtn')){
+
+            } else {
+                $(this).addClass('newBtn');
+                $('.modal:not([id])').addClass('notId fade resource-view-embed');
+                $('.modal.notId').wrapAll('<div class="modal-dialog"></div>');
+                $('.modal.notId').children().wrapAll('<div class="modal-dialog"></div>');
+                $('.modal.notId .modal-dialog').children().wrapAll('<div class="modal-content"></div>')
+            }
+        });
+    })
+
 });
